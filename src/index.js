3140a09f9b1bb678ccc7b0fdf8985eec9f8f4761
Rin Minase <rin.black.raison.detre@gmail.com>
2022-05-04 16:45:00 +0800

    Added links

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ import Perf from './Perf';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {
-      window.location.pathname === '/perf' ? (<Perf />) : (<App />)
-    }
+    <>
+      <div>
+        <a href="/">Home</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+        <a href="/">Performance Checking</a>
+      </div>
+
+      {
+        window.location.pathname === '/perf' ? (<Perf />) : (<App />)
+      }
+    </>
   </React.StrictMode>
 );
